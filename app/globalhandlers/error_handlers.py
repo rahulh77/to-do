9b51c9ebca0b4fa697626a836f117997c3e93ea5
rahul <rahul999@gmail.com>
2020-11-27 20:15:44 -0500

    added stack trace

--- app/globalhandlers/error_handlers.py
+++ app/globalhandlers/error_handlers.py
@@ -1,3 +1,4 @@
+import sys
 from app import app
 from flask import Response, request, current_app
 from app.globalhandlers import bp
@@ -5,15 +6,15 @@ from app.globalhandlers import bp
 
 @bp.app_errorhandler(404)
 def page_not_found(error):
-	current_app.logger.error('Page not found: %s', (request.path))
+	current_app.logger.error('Page not found: %s', (request.path), exc_info=sys.exc_info())
 	return Response(status=404)
 
 @bp.app_errorhandler(500)
 def internal_server_error(error):
-    current_app.logger.error('Server Error: %s', (error))
+    current_app.logger.error('Server Error: %s', (error), exc_info=sys.exc_info())
     return Response(status=500)
 
 @bp.app_errorhandler(Exception)
 def unhandled_exception(e):
-    current_app.logger.error('Unhandled Exception: %s', (e))
+    current_app.logger.error('Unhandled Exception: %s', (e), exc_info=sys.exc_info())
     return Response(status=500)
